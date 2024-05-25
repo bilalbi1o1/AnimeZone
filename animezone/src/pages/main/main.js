3c5166f87be54ff6f0cf21761402424469ebef2b
bilalbi1o1 <bilalba4oo4@gmail.com>
2024-05-25 06:33:58 -0700

    First Page

---
+++ animezone/src/pages/main/main.js
@@ -0,0 +1,19 @@
+import Header from './mainHeader';
+
+export default function Main()
+{
+    return ( 
+        <div>
+            <Header/>
+            <div className="mainContainer">
+                <div className='content'>
+                    <h1 className="webName" >AnimeZone</h1>
+                    <input className='searchBox' placeholder='Search anime...' ></input>
+                </div>
+                <img src='https://hianime.to/images/anw-min.webp?v=0.1' alt='homeImage' className="mainImage" />
+            </div>
+            <input className='searchBox' placeholder='Search anime...' ></input>
+
+        </div>
+    )
+}
---
+++ animezone/src/pages/main/mainHeader.js
@@ -0,0 +1,12 @@
+export default function Header()
+{
+    return(
+    <div className="Buttons" > 
+        <button className= "Button">Home</button>  
+        <button className= "Button" >Movies</button>   
+        <button className= "Button" >TV Series</button>   
+        <button className= "Button" >Most Popular</button>   
+        <button className= "Button" >Top Airing</button>   
+    </div>
+    )
+}
