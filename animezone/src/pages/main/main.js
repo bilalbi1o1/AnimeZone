2d1626f870d2b2f4ef792ed51a9bbea9ddba2bd9
Burhan Ahmad <burhanahmad1616j@gmail.com>
2024-05-26 22:02:40 +0500

    UI Changes

--- animezone/src/pages/main/main.js
+++ animezone/src/pages/main/main.js
@@ -1,4 +1,5 @@
 import Header from './mainHeader';
+import "./main.css";
 
 export default function Main()
 {
@@ -8,12 +9,10 @@ export default function Main()
             <div className="mainContainer">
                 <div className='content'>
                     <h1 className="webName" >AnimeZone</h1>
-                    <input className='searchBox' placeholder='Search anime...' ></input>
+                    <input className='searchBox' placeholder='    Search anime...' ></input>
                 </div>
                 <img src='https://hianime.to/images/anw-min.webp?v=0.1' alt='homeImage' className="mainImage" />
             </div>
-            <input className='searchBox' placeholder='Search anime...' ></input>
-
         </div>
     )
 }
